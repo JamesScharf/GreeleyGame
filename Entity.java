5545259fbb77bd4816a4c103936bbac33ff3db2e
Brian Smith <block8437@gmail.com>
2016-08-17 14:42:20 -0400

    Initial commit of the GameState and a partial implementation of the Entity class

---
+++ Entity.java
@@ -0,0 +1,19 @@
+public class Entity {
+    private boolean collidable;
+
+    public Entity() {
+
+    }
+
+    public void update() {
+        
+    }
+
+    public void setCollidable(boolean collidable) {
+        this.collidable = collidable;
+    }
+
+    public boolean isCollidable() {
+        return collidable;
+    }
+}
--- GameState.java
+++ GameState.java
@@ -1,11 +1,49 @@
 public class GameState {
-	public static void main (String[] args) {
+	private Entity array[][];
+	private int width;
+	private int height;
 
-    }
+	public GameState(int width, int height) {
+		this.array = new Entity[width][height];
+
+		this.width = width;
+		this.height = height;
+	}
+
+	public Entity set(int x, int y, Entity entity) {
+		if ( x < 0 || x >= this.width || y < 0 || y >= this.height ) {
+			return null;
+		}
+
+		if ( this.array[x][y] == null ) {
+			this.array[x][y] = entity;
+			return entity;
+		}
+		else {
+			if ( !this.array[x][y].isCollidable() ) {
+				Entity tmp = this.array[x][y];
+				this.array[x][y] = entity;
+				return tmp;
+			}
+			else {
+				return null;
+			}
+		}
+	}
+
+	public Entity get(int x, int y) {
+		if ( x < 0 || x >= this.width || y < 0 || y >= this.height ) {
+			return null;
+		}
+
+		return this.array[x][y];
+	}
+
+	public void update() {
+		for ( int x = 0; x < this.width; x++ ) {
+			for ( int y = 0; y < this.height; y++ ) {
+				this.array[x][y].update();
+			}
+		}
+	}
 }
-
-/*
-*
-*Handles game logic
-*
-*/
